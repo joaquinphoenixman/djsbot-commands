98befdf8f67341bdde2a960ff4631b5460a7f3db
joaquinphoenixman <58152959+joaquinphoenixman@users.noreply.github.com>
2019-12-01 14:46:16 +0100

    Update wgh.js

--- wgh.js
+++ wgh.js
@@ -1,11 +1,17 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+
 exports.run = (client, message, args) => {
   message.delete()
+  // deletes the message right after being run
+  
       let embed = new Discord.RichEmbed()
   .setImage("https://i.vgy.me/Oy2OV2.gif")
+      // You can set any gif you like, but the current one is of the meme
+      
   .setColor("GOLD")
   message.channel.send({embed});
+  //sends the embed 
 }
 
 // X = your bots prefix
