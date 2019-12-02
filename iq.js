d3d047d8bfd075f87ccd808918eee199899d4dc2
joaquinphoenixman <58152959+joaquinphoenixman@users.noreply.github.com>
2019-12-02 13:36:35 +0100

    Update iq.js

--- iq.js
+++ iq.js
@@ -1,21 +1,29 @@
-// Fun command to troll people
 
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-
-exports.run = async (client, message, args) => {
-    let user = message.mentions.users.first(); 
-    if (message.mentions.users === message.author.username) return message.reply('Your iq is 0 :)');
+    const Discord = require("discord.js");
+    const bot = new Discord.Client();
     
-    if (message.mentions.users.size < 1) return message.reply('Please mention someone')
-    
+    exports.run = async (client, message, args) => {
+        const iq = Math.floor(Math.random() * 201);
 
-    const iq = Math.floor(Math.random() * 201);
-    const embed = new Discord.RichEmbed()
-    .setColor("GOLD")
-    .setThumbnail("https://cdn0.iconfinder.com/data/icons/business-and-finance-29/244/icon-093-512.png")
-    .setTitle(user.username + " \nyour IQ is : " + iq)
-    message.channel.send(embed)
+        const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+        let target = message.mentions.users.first() || message.author
+        if (iq < 100) message.channel.send({"embed": {
+            // If the random number is below 100 aka iq < 100 it will display different text
+            
+      "color": 0xFFD700,
+      "title": member.user.username + "\nYour IQ is " + iq,
+      "thumbnail": {
+          "url": "https://cdn2.iconfinder.com/data/icons/emoticons-hand-drawn/81/Confused-512.png"
+
+    } 
+    }}); 
+        else message.channel.send({embed: {
+            // if iq is bigger then 100 it will display something else feel free to edit it
+            "color": 0xFFD700,
+            "title": member.user.username + "\nYour IQ is " + iq,
+            "thumbnail": {
+                "url": "https://icon-library.net/images/smart-icon/smart-icon-16.jpg"
+            }
+            }})
+
 }
-
-
